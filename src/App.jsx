0c0f0959a7b5f45f76135c9bf50067d4772cee0c
Benedict Halim <benedicthalim1@gmail.com>
2024-06-09 23:09:55 +0700

    im fixing browser router

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/ColossalNotebook">
         <Navbar />
         <Routes>
           <Route index element={<Home />} />
