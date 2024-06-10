94908b97a2759f3d17c1d66a8f1646a1de8138ec
Benedict Halim <benedicthalim1@gmail.com>
2024-06-10 10:16:45 +0700

    attempt 2 of fixing browserRouter's autism

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import Hierarchy from "./pages/Hierarchy";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/ColossalNotebook/",
+    element: <App />,
+    children: [
+      { path: "/ColossalNotebook/", element: <Home /> },
+      { path: "/ColossalNotebook/about", element: <About /> },
+      { path: "/ColossalNotebook/contact", element: <Contact /> },
+      { path: "/ColossalNotebook/hierarchy", element: <Hierarchy /> },
+      { path: "/ColossalNotebook/*", element: <NoPage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
